facf2fce84fdc65d3efdd01ed4cba04bacde8a9e
goofynugtz <rahulranjan25.rr@gmail.com>
2023-11-09 16:55:01 +0530

    INIT

--- index.js
+++ index.js
@@ -1,88 +1,46 @@
 const puppeteer = require("puppeteer-core");
 const chromium = require("@sparticuz/chromium");
-var exec = require('child_process').exec;
 
 var execPath;
 
 async function getPath(){
+  let startTime = performance.now();
+  
   if (execPath) return execPath;
   execPath = await chromium.executablePath()
+  console.log("Path: ", execPath)
+  
+  let endTime = performance.now();
+  let executionTime = endTime - startTime;
+  console.log(`Phase 1 [GetPath]: ${executionTime} ms`)
   return execPath;
 }
 
 async function func() {
-    // Phase 1
-  let startTime = performance.now();
 
   let browser = null;
   chromium.setHeadlessMode = true;
   chromium.setGraphicsMode = false;
-  
-  let endTime = performance.now();
-  let executionTime = endTime - startTime;
-  console.log(`Phase 1 [Init]: ${executionTime} ms`)
-/*   exec('ls', function (error, stdOut, stdErr) {
-    console.log(error)
-    console.log(stdOut)
-    console.log(stdErr)
-}); */
-  try {
-    // Phase 2
-    startTime = performance.now();
 
+  try {
     browser = await puppeteer.launch({
       args: chromium.args,
       executablePath: await getPath(),
       headless: chromium.headless,
       ignoreHTTPSErrors: true,
     });
-    
-    endTime = performance.now();
-    executionTime = endTime - startTime;
-    console.log(`Phase 2 [Browser Invoke]: ${executionTime} ms`)
-    
-    // Phase 3
-    startTime = performance.now();
-    
-    const page = await browser.newPage();
-    
-    endTime = performance.now();
-    executionTime = endTime - startTime;
-    console.log(`Phase 3 [Create new page]: ${executionTime} ms`)
 
-    // Phase 4
-    startTime = performance.now();
-    
-    await page.goto('https://example.com');
-    
-    endTime = performance.now();
-    executionTime = endTime - startTime;
-    console.log(`Phase 4 [URL fetch]: ${executionTime} ms`)
-
-    // Phase 5
-    startTime = performance.now();
-    
-    await page.screenshot({ path: '/tmp/screenshot.png' });
-    
-    endTime = performance.now();
-    executionTime = endTime - startTime;
-    console.log(`Phase 5 [Screen Capture]: ${executionTime} ms`)
-
+    await page.goto('file://home/goofynugtz/Work/HomeDrop/pupp-poc/templates/gst-invoice.html');
+    await page.screenshot({ path: 'screenshot.png' });
     return 'Screenshot captured';
 
   } catch (error) {
     console.error('Error:', error);
     throw error;
+
   } finally {
     if (browser !== null) {
-      // Phase 6
-      startTime = performance.now();
-      
       await browser.close();
-      
-      endTime = performance.now();
-      executionTime = endTime - startTime;
-      console.log(`Phase 6 [Browser Dealloc]: ${executionTime} ms`)
     }
   }
 };
