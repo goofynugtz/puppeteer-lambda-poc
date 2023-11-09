4b46aec7949b740c3c556af08918f2a4beba7ea7
goofynugtz <rahulranjan25.rr@gmail.com>
2023-11-09 11:37:46 +0530

    dummy commit

--- index.js
+++ index.js
@@ -3,59 +3,55 @@ const chromium = require("@sparticuz/chromium-min");
 var exec = require('child_process').exec;
 
 exports.handler = async (event, context) => {
-    // Phase 1
+  // Phase 1
   let startTime = performance.now();
 
   let browser = null;
   chromium.setHeadlessMode = true;
   chromium.setGraphicsMode = false;
-  
+
   let endTime = performance.now();
   let executionTime = endTime - startTime;
   console.log(`Phase 1 [Init]: ${executionTime} ms`)
-  exec('ls', function (error, stdOut, stdErr) {
-    console.log(error)
-    console.log(stdOut)
-    console.log(stdErr)
-})  
+
   try {
     // Phase 2
     startTime = performance.now();
 
     browser = await puppeteer.launch({
       args: chromium.args,
-      executablePath: await chromium.executablePath("/opt/chromium"),
+      executablePath: await chromium.executablePath(),
       headless: chromium.headless,
       ignoreHTTPSErrors: true,
     });
-    
+
     endTime = performance.now();
     executionTime = endTime - startTime;
     console.log(`Phase 2 [Browser Invoke]: ${executionTime} ms`)
-    
+
     // Phase 3
     startTime = performance.now();
-    
+
     const page = await browser.newPage();
-    
+
     endTime = performance.now();
     executionTime = endTime - startTime;
     console.log(`Phase 3 [Create new page]: ${executionTime} ms`)
 
     // Phase 4
     startTime = performance.now();
-    
+
     await page.goto('https://example.com');
-    
+
     endTime = performance.now();
     executionTime = endTime - startTime;
     console.log(`Phase 4 [URL fetch]: ${executionTime} ms`)
 
     // Phase 5
     startTime = performance.now();
-    
+
     await page.screenshot({ path: '/tmp/screenshot.png' });
-    
+
     endTime = performance.now();
     executionTime = endTime - startTime;
     console.log(`Phase 5 [Screen Capture]: ${executionTime} ms`)
@@ -69,9 +65,9 @@ exports.handler = async (event, context) => {
     if (browser !== null) {
       // Phase 6
       startTime = performance.now();
-      
+
       await browser.close();
-      
+
       endTime = performance.now();
       executionTime = endTime - startTime;
       console.log(`Phase 6 [Browser Dealloc]: ${executionTime} ms`)
